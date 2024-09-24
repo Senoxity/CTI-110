831a529534b2bf5dc494f3d2579783663e93b5ff
Senoxity <canadya3600@student.faytechcc.edu>
2024-09-24 14:18:15 -0400

    Add files via upload

---
+++ P1LAB1_CanadyAiden.py
@@ -0,0 +1,13 @@
+# Aiden Canady
+# 9/24/24
+# P1LAB1
+# It shows how to get input from the user and display output.
+
+# Get first name from user
+firstName = input("Enter your first name: ")
+print()
+# Get last name from user
+lastName = input("Enter your last name: ")
+print()
+# Display info to user
+print("Hello,", firstName, lastName + "! Welcome to CTI-110!")
