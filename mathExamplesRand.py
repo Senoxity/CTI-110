3f9893c56299da3f2df50e6771f772434c4de71d
Senoxity <canadya3600@student.faytechcc.edu>
2024-10-03 13:49:53 -0400

    Add files via upload

---
+++ mathExamplesRand.py
@@ -0,0 +1,13 @@
+# Aiden Canady
+# complex math equation examples
+
+# import random library
+import random
+
+# pythag theorem variables and math
+side1 = random.randint(1,100)
+side2 = random.randint(1,100)
+hypo = (side1 ** 2) + (side2 ** 2)
+
+# display output to user
+print(f"The hypotenuse, where a is {side1} and b is {side2}, is {hypo}.")
